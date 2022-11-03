619f3db3bb6569fdc4bf0ff7b059bb565c077566
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-11-02 20:19:19 -0400

    add seo image for open graph and twitter

---
+++ satori.jsx
@@ -0,0 +1,47 @@
+<div
+  style={{
+    display: 'flex',
+    height: '100%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    letterSpacing: '-.02em',
+    fontWeight: 700,
+    background: 'white',
+  }}
+>
+  <div
+    style={{
+      left: 42,
+      top: 42,
+      position: 'absolute',
+      display: 'flex',
+      alignItems: 'center',
+    }}
+  >
+    <img src="https://media.licdn.com/dms/image/C4E03AQHal_cwvvrNcg/profile-displayphoto-shrink_200_200/0/1623796430789?e=1672876800&v=beta&t=9cVhe54M5LeKRfPfBUdNRY1IOCS4XOWccBOR_YwCXz8" width={30} />
+    <span
+      style={{
+        marginLeft: 8,
+        fontSize: 20,
+      }}
+    >
+      blog.dpanovitch.dev
+    </span>
+  </div>
+  <div
+    style={{
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+      padding: '20px 50px',
+      margin: '0 42px',
+      fontSize: 40,
+      width: 'auto',
+      textAlign: 'center',
+      lineHeight: 1.4,
+    }}
+  >
+    Moved by curiosity.
+  </div>
+</div>
